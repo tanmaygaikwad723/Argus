d0c73ab6a2ce39f8b06b0f8070cdf082b169db14
tanmaygaikwad723 <gaikwadtanmay723@gmail.com>
2026-08-15 01:14:30 +0530

    feat: add the filter by date feature to query by actor and event words tool, add query by location and event words tool

--- agent/agent_tools.py
+++ agent/agent_tools.py
@@ -1,7 +1,7 @@
 from langchain_core.tools import BaseTool
-from schemas.llm_query import QueryByActor, QueryByActorNameandWords
+from schemas.llm_query import QueryByActor, QueryByActorNameandWords, QueryByLocationandwords
 from pydantic import BaseModel
-from agent.semantic_layer import query_by_actor, query_by_actor_and_eventword, store_event_article_pairs
+from agent.semantic_layer import query_by_actor, query_by_actor_and_eventword, query_by_event_and_location
 from typing import Type, List, Optional
 from datetime import date as date_type, datetime
 
@@ -43,20 +43,19 @@ class QueryByActorTool(BaseTool):
         before = safe_parse_date(occured_before)
         on     = safe_parse_date(occured_on)
 
-        response = query_by_actor(
+        return query_by_actor(
             actor_name,
             occured_after=after,
             occured_before=before,
             occured_on=on,
-        )
-        events_list = store_event_article_pairs(response)
-        return events_list
+        ).result_set
 
 
 class QueryByActorAndWordsTool(BaseTool):
     name: str = "Query_by_actor_names_and_words"
     description: str = (
-        "Useful when you have information about actors as well as certain words that are related to event to query information about event."
+        "Use this tool when you have information about actors as well as certain words that are related to event."
+        "Optionally filtered by date range or exact date."
     )
     args_schema: Type[BaseModel] = QueryByActorNameandWords
 
@@ -64,10 +63,50 @@ class QueryByActorAndWordsTool(BaseTool):
             self,
             actor_names:List[str],
             words: List[str],
+            occured_on: Optional[str] = None,
+            occured_before: Optional[str] = None,
+            occured_after: Optional[str] = None,
     )->List[dict]:
         """Using the tool"""
-        response = query_by_actor_and_eventword(actor_names, words)
-        events_list = store_event_article_pairs(response)
-        return events_list
+        on = safe_parse_date(occured_on)
+        before = safe_parse_date(occured_before)
+        after = safe_parse_date(occured_after)
+        return query_by_actor_and_eventword(
+            actor_names, 
+            words,
+            occured_on=on,
+            occured_before=before,
+            occured_after=after
+            ).result_set
+
+
+class QueryByLocationandWordsTool(BaseTool):
+    name: str = "Query_by_eventwords_and_location"
+    description: str = (
+    "Use this tool when you have information about the location where the event happened as well as certain words that are related to event."
+    "Optionally filtered by date range or exact date"
+    )
+    args_schema: Type[BaseModel] = QueryByLocationandwords
+
+    def _run(
+            self,
+            event_words: List[str],
+            location: str,
+            occured_on: Optional[str] = None,
+            occured_before: Optional[str] = None,
+            occured_after: Optional[str] = None,
+    ) -> List[dict]:
+        """Using the tool"""
+        on = safe_parse_date(occured_on)
+        before = safe_parse_date(occured_before)
+        after = safe_parse_date(occured_after)
+        return query_by_event_and_location(
+            event_words, 
+            location,
+            occured_after=after, 
+            occured_before=before, 
+            occured_on=on
+            ).result_set
+
     
     

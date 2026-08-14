2110e92e69f3f2823653876afeeb2728f254a90c
tanmaygaikwad723 <gaikwadtanmay723@gmail.com>
2026-08-15 01:20:26 +0530

    feat: create a DateFilterMixin class to add common date filter parameters to pydantic models, add QueryByLocationandwords pydantic model, create child class of DataFilterMixin model

--- schemas/llm_query.py
+++ schemas/llm_query.py
@@ -1,29 +1,41 @@
 from pydantic import BaseModel, Field
-from typing import List, Optional
+from typing import List, Optional, Generic, TypeVar
 
 
-class QueryByActor(BaseModel):
-    actor_name: str = Field(description="The name of actor of event. It can be a country, indiviual or organization")
+
+class DateFilterMixin(BaseModel):
     occured_after: Optional[str] = Field(
         default=None,
         description=(
-            "Only include events after this date, formatted YYYY-MM-DD. "
+            "Only include events happened/occured after this date, formatted YYYY-MM-DD."
             "Omit this parameter entirely if the user did not mention a start date."
         ),
     )
     occured_before: Optional[str] = Field(
         default=None,
         description=(
-            "Only include events before this date, formatted YYYY-MM-DD. "
-            "Omit this parameter entirely if the user did not mention an end date."
+            "Only include events happened/occured before this date, formatted YYYY-MM-DD."
+            "Omit this parameter entirely if the user did not mention a end date."
         ),
     )
     occured_on: Optional[str] = Field(
         default=None,
-        description="Only include events on this exact date, formatted YYYY-MM-DD. Omit if not specified.",
+        description=(
+            "Only include events happened/occured on this data, formatted YYYY-MM-DD."
+            "Omit this parameter entirely if the user did not mention a date."
+        )
     )
 
 
-class QueryByActorNameandWords(BaseModel):
+class QueryByActor(DateFilterMixin):
+    actor_name: str = Field(description="The name of actor of event. It can be a country, indiviual or organization")
+
+
+class QueryByActorNameandWords(DateFilterMixin):
     actor_names: List[str] = Field(description="The name of actors who participated in the event. It can be a country, an individual  or an organization")
     words: List[str] = Field(description="The words or specific phrases that are related to the event.")
+
+    
+class QueryByLocationandwords(DateFilterMixin):
+    event_words : List[str] = Field("The word or specific phrases that are related to the event.")
+    location: str = Field("The name of the location where the event has occured.")

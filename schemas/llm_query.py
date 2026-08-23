a1f9d41cd4aa72b76609eb949b93da4e16080074
tanmaygaikwad723 <gaikwadtanmay723@gmail.com>
2026-08-23 20:19:09 +0530

    Define 'EventWordsFilterMixin' pydantic model class, define 'QueryByLocation' and 'QueryByEventwords' pydantic model

--- schemas/llm_query.py
+++ schemas/llm_query.py
@@ -27,15 +27,27 @@ class DateFilterMixin(BaseModel):
     )
 
 
+class EventWordsFilterMixin(BaseModel):
+    event_words: List[str] = Field("The word or specific phrases that are related to the event.")
+    all: Optional[bool] = Field(default=True, description="Boolean field that indicates whether the event summary should " \
+                                                "contain all the event words of the list or any one of them.")
+
+
 class QueryByActor(DateFilterMixin):
     actor_name: str = Field(description="The name of actor of event. It can be a country, indiviual or organization")
 
 
-class QueryByActorNameandWords(DateFilterMixin):
+class QueryByActorNameandWords(DateFilterMixin, EventWordsFilterMixin):
     actor_names: List[str] = Field(description="The name of actors who participated in the event. It can be a country, an individual  or an organization")
-    words: List[str] = Field(description="The words or specific phrases that are related to the event.")
 
     
-class QueryByLocationandwords(DateFilterMixin):
-    event_words : List[str] = Field("The word or specific phrases that are related to the event.")
+class QueryByLocationandwords(DateFilterMixin, EventWordsFilterMixin):
     location: str = Field("The name of the location where the event has occured.")
+
+
+class QueryByEventwords(DateFilterMixin,EventWordsFilterMixin):
+    pass
+
+
+class QueryByLocation(DateFilterMixin):
+    location: str = Field(description="The name of the location where the event has occured")
